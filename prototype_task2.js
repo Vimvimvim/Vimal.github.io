61277f6937e52cade1ddbb9f92b88904cec07e58
Vimvimvim <vimal.bellamkonda@gmail.com>
2017-02-20 15:06:31 -0600

    vimvimvim.github.io

---
+++ prototype_task2.js
@@ -0,0 +1,8 @@
+var titleBox = document.getElementById("titleBox");
+
+
+
+var titleButton = document.getElementById("title");
+titleButton.onclick = function(e) {
+  window.location.href = "prototype_mainmenu.html";
+}
